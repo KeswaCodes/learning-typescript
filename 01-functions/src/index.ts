a8f3a3ce287dde357f64009a34ad1494c719ad12
Zinhle <zkeswa023@student.wethinkcode.co.za>
2025-01-11 04:06:48 +0200

    Updated script & changed directory structure

---
+++ 01-functions/src/index.ts
@@ -0,0 +1,5 @@
+function greetUser(name : string): void {
+    console.log(`Hello there ${name}`);
+} 
+
+greetUser("Zinhle");
--- typescript-setup.sh
+++ typescript-setup.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-echo "Initialising project..."
+echo "Initialising project with package.json file..."
 npm init -y
 
-    echo "Installing typescript..."
-npm install typescript --save-dev
-
-echo "Adding configuration for TypeScript"
-tsc --init
-
-echo "Adding Jest as a test dependency..."
-npm install jest --save-dev
-
-
-echo "Installing jest package..."
-npm install ts-jest --save-dev
-
-echo "Installing type defintions for jest"
-npm install @types/jest --save-dev
-
+mkdir src
+mkdir tests
