74136c8562099cb8f391cc575f455201d1ed4bca
Zinhle <zkeswa023@student.wethinkcode.co.za>
2025-01-14 08:38:20 +0200

    Got frontend content to change at intervals

--- 02-mastermind-assembly/out/get-input.js
+++ 02-mastermind-assembly/out/get-input.js
@@ -1,15 +1,40 @@
-// "use strict";
+"use strict";
 /**
  * This module deals with all the input & output operations of the game
  * welcomeUser() welcomes user to the game & introduces controls & rules
  * getUserInput() gets input from user and returns the value
  */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// function welcomeUser(): void {
+//     console.log("Welcome to the Mastermind game...");
+//     console.log("Here are the rules...");
+//     console.log("Play well and enjoy! ");
+// }
 function welcomeUser() {
-    console.log("Welcome to the Mastermind game...");
-    console.log("Here are the rules...");
-    console.log("Play well and enjoy! ");
+    return __awaiter(this, void 0, void 0, function* () {
+        let listPhrases = ["Welcome to the Mastermind game...", "Here are the rules...", "Play well and enjoy! "];
+        var element = document.getElementById("main-heading");
+        for (let phrase of listPhrases) {
+            console.log(phrase);
+            if (element) {
+                element.textContent = phrase;
+            }
+            yield sleep(2000);
+        }
+    });
 }
 function getUserInput() {
     let letter = prompt("What letter do you think is missing from the word?: ");
     return letter == null ? letter || " " : " ";
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
--- 02-mastermind-assembly/src/get-input.ts
+++ 02-mastermind-assembly/src/get-input.ts
@@ -7,14 +7,31 @@
 
 
 
-function welcomeUser(): void {
-    console.log("Welcome to the Mastermind game...");
-    console.log("Here are the rules...");
-    console.log("Play well and enjoy! ");
+// function welcomeUser(): void {
+//     console.log("Welcome to the Mastermind game...");
+//     console.log("Here are the rules...");
+//     console.log("Play well and enjoy! ");
+// }
+
+
+async function welcomeUser(){
+
+    let listPhrases = ["Welcome to the Mastermind game...", "Here are the rules...", "Play well and enjoy! "];
+    var element = document.getElementById("main-heading");
+    for(let phrase of listPhrases) {
+        console.log(phrase);
+        if(element) {element.textContent = phrase;}
+        await sleep(2000);
+    }
+
 }
 
 
 function getUserInput() : string {
     let letter = prompt("What letter do you think is missing from the word?: ");
     return letter == null ? letter || " " : " ";
+}
+
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
